f51989f920300db416bce73d48a9be9b2a46b4f0
Quadri4423 <isholaolateju2015@gmail.com>
2026-03-30 13:20:31 +0100

    update

--- app/components/Navigation/Logo.vue
+++ app/components/Navigation/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <button @click="$emit('logoClicked')" class="flex items-center font-bold gap-3 text-2xl text-secondary marcellus">
     <!-- <img src="@/assets/images/unilorin.png" alt="Logo" class=" w-12 object-contain" /> -->
-      tp
+      Abanise
 
       <div class=" relative  t right-2 h-2 w-2  bg-primary  rounded-full"></div>
      </button>
